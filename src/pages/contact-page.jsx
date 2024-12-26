44d60e44ee5eb397c09a8342dac699272cc4551b
TaskReminder <21104023@ittelkom-pwt.ac.id>
2024-12-26 16:00:30 +0700

    update

--- src/pages/contact-page.jsx
+++ src/pages/contact-page.jsx
@@ -11,7 +11,7 @@ export default function ContactPage() {
     <main className="flex w-full flex-col items-center justify-center gap-10 py-10">
       <div className="mx-auto flex w-full max-w-[1440px] flex-col gap-20 px-20">
         <h1 className="text-center text-2xl font-semibold md:text-4xl">
-          Hubungi 
+          Hubungi Kami
         </h1>
 
         <div className="flex columns-2 flex-wrap items-center justify-center gap-5 px-5">
